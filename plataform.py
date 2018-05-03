af133c3299c65ec58c38aaf344bbf6f6713e312b
AmandaRM <amandaruma@gmail.com>
2018-05-03 16:50:09 -0300

    acrescimo de plataforma
    as plataformas foram um grupo
    diminui tamanho plataforma e bonzinho

---
+++ plataform.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 27 16:52:20 2018
+
+@author: Amanda
+"""
+import pygame
+
+class Plataform (pygame.sprite.Sprite):
+    
+    def __init__(self, arquivo_imagem, dimension):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect()
+        self.rect.x = dimension[0]
+        self.rect.y = dimension[1]
