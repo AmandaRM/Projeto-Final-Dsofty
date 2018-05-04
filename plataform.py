10e01fee3932a275e822a31fac8e6c28d1027cb7
AmandaRM <amandaruma@gmail.com>
2018-05-04 09:18:58 -0300

    plataformas não aleatorias

--- plataform.py
+++ plataform.py
@@ -16,7 +16,7 @@ class Plataform (pygame.sprite.Sprite):
         self.rect.x = dimension[0]
         self.rect.y = dimension[1]
         
-def cria_Plataform():
+def cria_Plataform_Aleatoria():
     plataformas_group=pygame.sprite.Group()
     i=0
     while i<5:
@@ -26,3 +26,18 @@ def cria_Plataform():
         plataformas_group.add(plataforma1)
         i+=1
     return plataformas_group
+
+def cria_Plataform_nAleatoria():
+     plataformas_group=pygame.sprite.Group()
+     plataforma1=Plataform("plataform_de_pedra_reta_pequena.png", [100, 500])        
+     plataformas_group.add(plataforma1)
+     plataforma2=Plataform("plataform_de_pedra_reta_pequena.png", [200, 500])        
+     plataformas_group.add(plataforma2)
+     plataforma3=Plataform("plataform_de_pedra_reta_pequena.png", [300, 500])        
+     plataformas_group.add(plataforma3)
+     plataforma4=Plataform("plataform_de_pedra_reta_pequena.png", [400, 500])        
+     plataformas_group.add(plataforma4)
+     plataforma5=Plataform("plataform_de_pedra_reta_pequena.png", [500, 500])        
+     plataformas_group.add(plataforma5)
+     return plataformas_group
+     
