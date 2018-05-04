de40fb60c006c11f22b1804e6d820eeaf4188fe4
AmandaRM <amandaruma@gmail.com>
2018-05-04 19:41:58 -0300

    acrescentei mais plataformas e concertei o erro de movimentação dos bonequinhos
    os bonequinhos não tem a opção descer, já que vamos implementar a gravidade
    as plataformas foram acrescentadas manualmente, então só tem esse cenário disponível, teríamos que pensar em como fazer para que um loop faça essa inclusão

--- movimento.py
+++ movimento.py
@@ -7,18 +7,9 @@ Created on Fri May  4 08:03:19 2018
 import pygame
 
 # ===============      CLASSES      ===============
-class bonzinho(pygame.sprite.Sprite):
-  
-  def __init__(self, arquivo_imagem, pos_x, pos_y):
-    pygame.sprite.Sprite.__init__(self)
-    self.image = pygame.image.load(arquivo_imagem)
-    self.rect = self.image.get_rect()
-    self.rect.x = pos_x
-    self.rect.y = pos_y
     
- 
-# ===============      CLASSES      ===============
-class malvado(pygame.sprite.Sprite):
+
+class bonequinho(pygame.sprite.Sprite):
     def __init__(self, arquivo_imagem, pos_x, pos_y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(arquivo_imagem)
@@ -26,12 +17,14 @@ class malvado(pygame.sprite.Sprite):
         self.rect.x = pos_x
         self.rect.y = pos_y
 
-####### fazer funções em que cada função é um movimento #####
-def bom_UP():
-    bonzinho.rect.y-=1
+    def bom_UP(self):
+        self.rect.move_ip(0,-2) #move_ip(x,y)
+        return self
 
-def bom_LEFT():
-    bonzinho.rect.x-=1
+    def bom_LEFT(self):
+        self.rect.move_ip(-1,0) #move_ip(x,y)
+        return self
 
-def bom_RIGHT():
-    bonzinho.rect.x+=1
+    def bom_RIGHT(self):
+        self.rect.move_ip(1,0) #move_ip(x,y)
+        return self
