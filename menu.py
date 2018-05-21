5e6f7c7a97461f8b9edc3d069a9567ca8d39ae2c
AmandaRM <amandaruma@gmail.com>
2018-05-21 18:40:07 -0300

    começou a inserir o start game - manuela

--- menu.py
+++ menu.py
@@ -14,9 +14,15 @@ pygame.init()
 tela2 = pygame.display.set_mode((800, 600), 0, 32)
 pygame.display.set_caption('Star Lego')
 fundo = pygame.image.load("Fundo-Estrelas.jpg").convert()
+iniciar_jogo = movimento.bonequinho("startgame.png", 125, 300)
+iniciar_jogo_group=pygame.sprite.Group()
+
+iniciar_jogo_group.add(iniciar_jogo)
 nome_jogo= movimento.bonequinho("starlego.png", 125, 180)
 nome_jogo_group=pygame.sprite.Group()
 nome_jogo_group.add(nome_jogo)
+
+
 
 tela2.blit(fundo, (0, 0))
 nome_jogo_group.draw(tela2)
