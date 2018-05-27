f2aca05017076ca985c3ac9a9f9053ba5491d32e
AmandaRM <amandaruma@gmail.com>
2018-05-26 21:09:44 -0300

    letras pra por nome

--- username.py
+++ username.py
@@ -15,19 +15,16 @@ yellow = (255,255,0)
 ACCEPTED = string.ascii_letters+string.digits+string.punctuation+" "
 
 def inserir_nome():
-    nome=[]
+    nome="usuario"
+    nome_inserido=" "
      #pegar o nome do usuário
     for event in pygame.event.get():
          if event.type == pygame.KEYDOWN:
              if event.key == pygame.K_RETURN:
                  return nome
              elif event.key == pygame.K_BACKSPACE:
-                 nome = nome[:-1]
+                 nome_inserido = nome_inserido[:-1]
              elif event.unicode in ACCEPTED:
-                 if len(nome) < 11:
-                     nome += event.unicode
-    return nome
-     
-    
-pygame.init()
-tecla_pressionada = pygame.key.get_pressed()
+                 if len(nome_inserido) < 12:
+                     return event.unicode
+ 
